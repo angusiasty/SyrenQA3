44c602da9394148d42de1f8dca8023e3685dd65c
Maciej Andrzejczyk <maciej.andrzejczyk@smtsoftware.com>
2017-06-20 12:40:30 +0200

    Add sample solution

---
+++ SyrenQA3/SyrenQA3/Training.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyrenQA3
+{
+	public class Training
+	{
+		private List<int> mylist;
+		private Random rnd = new Random();
+
+		public List<int> FillMyList(int maxNumber)
+		{
+			mylist = new List<int> { rnd.Next(10, 20) };
+			bool wasElementOnList;
+
+			for (int currentNumber = 1; currentNumber <= maxNumber; currentNumber++)
+			{
+				wasElementOnList = false;
+				foreach (int element in mylist)
+				{
+					if (currentNumber == element)
+					{
+						wasElementOnList = true;
+						Console.WriteLine("{0} - this was random number", currentNumber);
+						break;
+
+					}
+					continue;
+				}
+				if (!wasElementOnList)
+				{
+					mylist.Add(currentNumber);
+					Console.WriteLine(currentNumber);
+				}
+			}
+
+			return mylist;
+		}
+	}
+}
